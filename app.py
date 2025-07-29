14c80a560907f14dfe092dc3eea79298756d367c
Łukasz Lesisz <lukasz.lesisz@adtran.com>
2025-07-29 14:49:10 +0200

    Aktualizacja: zmiany w app.py, nowa struktura projektu

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index_bootstrap.html")  # lub 'index.html'
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
